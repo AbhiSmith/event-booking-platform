a30ceb345f65e00ba70ee5304df376ebbabccd27
AbhiSmith <abhishekv4004@gmail.com>
2024-01-26 17:37:02 +0530

    clerk instance  update create delete done and database

---
+++ components/ui/shared/MobileNav.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from 'next/image'
+import NavItems from './NavItems'
+import { Separator } from "@/components/ui/separator"
+
+  
+
+const MobileNav = () => {
+  return (
+    <nav className='md:hidden'>
+        <Sheet>
+            <SheetTrigger className='align-middle'>
+                <Image src="/assets/icons/menu.svg" alt="menu" width={24} height={24} className="cursor-pointer"/>
+            </SheetTrigger>
+            <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+                <Image src="/assets/images/logo.svg" alt="Evently logo" width={128} height={38}/>
+                <Separator className="border border-gray-50" />
+                <NavItems />
+            </SheetContent>
+            </Sheet>
+
+    </nav>
+  )
+}
+
+export default MobileNav
